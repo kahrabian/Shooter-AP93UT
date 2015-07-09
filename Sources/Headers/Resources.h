f195c8f0ce3e064a2b7ceb673bd41a8212c5f65b
Kian Ahrabian <kahrabian@yahoo.com>
2015-07-09 11:06:03 +0430

    Project Update

---
+++ Sources/Headers/Resources.h
@@ -0,0 +1,13 @@
+//
+// Created by kahrabian on 7/9/15.
+//
+
+#ifndef SHOOTER_AP93UT_RESOURCES_H
+#define SHOOTER_AP93UT_RESOURCES_H
+
+#endif //SHOOTER_AP93UT_RESOURCES_H
+
+namespace res
+{
+
+}
---
+++ Sources/Headers/SettingData.h
@@ -0,0 +1,25 @@
+//
+// Created by kahrabian on 7/9/15.
+//
+
+#ifndef SHOOTER_AP93UT_SETTINGDATA_H
+#define SHOOTER_AP93UT_SETTINGDATA_H
+
+
+struct SettingData
+{
+    static int gMode;
+    static int gDiff;
+    static int uShp;
+    static int aln;
+    static int env;
+    static int mVol;
+    static bool mMut;
+    static int sfVol;
+    static bool sfMut;
+    static int aiDiff;
+    static int aiShp;
+};
+
+
+#endif //SHOOTER_AP93UT_SETTINGDATA_H
