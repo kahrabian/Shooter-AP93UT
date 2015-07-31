dad43646e0121fa5e14d5aff740bb76d2b744b75
Kian Ahrabian <kahrabian@ut.ac.ir>
2015-08-01 00:21:42 +0430

    Project Update

--- Sources/Source Files/MyAi.cpp
+++ Sources/Source Files/MyAi.cpp
@@ -39,94 +39,138 @@ void MyAi::gnrt_cmmnds() {
 				nrst_itm = i;
 			}
 		}
+	bool up = false, down = false, left = false, right = false;
 	if (nrst_itm != shp && nrst_itm->pos().x() < shp->scene()->views().first()->sceneRect().x() +
 			                                             (3.0 * shp->scene()->views().first()->sceneRect().width() /
 			                                              4)) {
 		if (shp->sceneBoundingRect().right() - shp->boundingRect().width() / 2 <
 		    nrst_itm->sceneBoundingRect().left() +
 				    nrst_itm->boundingRect().width() / 2 - MyRes::ai_view_offset) {
-			cmmnds->insert(-Qt::Key_D);
+//			cmmnds->insert(-Qt::Key_D);
+			right = true;
 		}
 		else if (shp->sceneBoundingRect().left() + shp->boundingRect().width() / 2 >
 				nrst_itm->sceneBoundingRect().right() - nrst_itm->boundingRect().width() / 2 + MyRes::ai_view_offset) {
-			cmmnds->insert(-Qt::Key_A);
+//			cmmnds->insert(-Qt::Key_A);
+			left = true;
 		}
 		if (shp->sceneBoundingRect().bottom() - shp->boundingRect().height() / 2 <
 		    nrst_itm->sceneBoundingRect().top() +
 				    nrst_itm->boundingRect().height() / 2 - MyRes::ai_view_offset) {
-			cmmnds->insert(-Qt::Key_S);
+//			cmmnds->insert(-Qt::Key_S);
+			down = true;
 		}
 		else if (shp->sceneBoundingRect().top() + shp->boundingRect().height() / 2 >
 				nrst_itm->sceneBoundingRect().bottom() - nrst_itm->boundingRect().height() / 2 +
 				MyRes::ai_view_offset) {
-			cmmnds->insert(-Qt::Key_W);
+//			cmmnds->insert(-Qt::Key_W);
+			up = true;
 		}
 		if ((nrst_itm->pos().y() < MyRes::y_offset ||
 		     nrst_itm->pos().y() > shp->scene()->views().first()->viewport()->rect().height() - MyRes::y_offset) &&
 		    nrst_itm->pos().x() > shp->pos().x()) {
-			cmmnds->insert(-Qt::Key_D);
+//			cmmnds->insert(-Qt::Key_D);
+			right = true;
 		}
 	}
 	else {
-		cmmnds->insert(-Qt::Key_A);
+//		cmmnds->insert(-Qt::Key_A);
+		left = true;
 	}
-	bool up = true, down = true, left = true, right = true;
 	itms = shp->scene()->items(shp->sceneBoundingRect().x() + shp->sceneBoundingRect().width() / 2 - 100,
 	                           shp->sceneBoundingRect().y() + shp->sceneBoundingRect().height() / 2 - 100, 200, 200,
 	                           Qt::IntersectsItemShape, Qt::AscendingOrder);
 			foreach (QGraphicsItem *i, itms) {
-			if (dynamic_cast<MyAlien *>(i)) {
-				if (dynamic_cast<MyAlien *>(i)->sceneBoundingRect().y() +
-				    dynamic_cast<MyAlien *>(i)->pixmap().height() / 2 >
-				    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
-					up = false;
+			if (!shp->shpshld->isVisible()) {
+				if (dynamic_cast<MyAlien *>(i)) {
+					if (dynamic_cast<MyAlien *>(i)->sceneBoundingRect().y() +
+					    dynamic_cast<MyAlien *>(i)->pixmap().height() / 2 >
+					    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
+						up = false;
+					}
+					else {
+						down = false;
+					}
+					if (dynamic_cast<MyAlien *>(i)->sceneBoundingRect().x() +
+					    dynamic_cast<MyAlien *>(i)->pixmap().width() / 2 >
+					    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
+						right = false;
+					}
+					else {
+						left = false;
+					}
 				}
-				else {
-					down = false;
+				else if (dynamic_cast<MyAsteroid *>(i)) {
+					if (dynamic_cast<MyAsteroid *>(i)->sceneBoundingRect().y() +
+					    dynamic_cast<MyAsteroid *>(i)->pixmap().height() / 2 >
+					    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
+						up = false;
+					}
+					else {
+						down = false;
+					}
+					if (dynamic_cast<MyAsteroid *>(i)->sceneBoundingRect().x() +
+					    dynamic_cast<MyAsteroid *>(i)->pixmap().width() / 2 >
+					    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
+						right = false;
+					}
+					else {
+						left = false;
+					}
 				}
-				if (dynamic_cast<MyAlien *>(i)->sceneBoundingRect().x() +
-				    dynamic_cast<MyAlien *>(i)->pixmap().width() / 2 >
-				    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
-					right = false;
+				else if (dynamic_cast<MyBullet *>(i) && dynamic_cast<MyBullet *>(i)->getDir() == -1) {
+					if (dynamic_cast<MyBullet *>(i)->sceneBoundingRect().y() +
+					    dynamic_cast<MyBullet *>(i)->pixmap().height() / 2 >
+					    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
+						up = false;
+					}
+					else {
+						down = false;
+					}
+					if (dynamic_cast<MyBullet *>(i)->sceneBoundingRect().x() +
+					    dynamic_cast<MyBullet *>(i)->pixmap().width() / 2 >
+					    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
+						right = false;
+					}
+					else {
+						left = false;
+					}
 				}
-				else {
-					left = false;
+				else if (dynamic_cast<MyShip *>(i) && i != shp) {
+					if (dynamic_cast<MyShip *>(i)->sceneBoundingRect().y() +
+					    dynamic_cast<MyShip *>(i)->pixmap().height() / 2 >
+					    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
+						up = false;
+					}
+					else {
+						down = false;
+					}
+					if (dynamic_cast<MyShip *>(i)->sceneBoundingRect().x() +
+					    dynamic_cast<MyShip *>(i)->pixmap().width() / 2 >
+					    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
+						right = false;
+					}
+					else {
+						left = false;
+					}
 				}
-			}
-			else if (dynamic_cast<MyAsteroid *>(i)) {
-				if (dynamic_cast<MyAsteroid *>(i)->sceneBoundingRect().y() +
-				    dynamic_cast<MyAsteroid *>(i)->pixmap().height() / 2 >
-				    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
-					up = false;
-				}
-				else {
-					down = false;
-				}
-				if (dynamic_cast<MyAsteroid *>(i)->sceneBoundingRect().x() +
-				    dynamic_cast<MyAsteroid *>(i)->pixmap().width() / 2 >
-				    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
-					right = false;
-				}
-				else {
-					left = false;
-				}
-			}
-			else if (dynamic_cast<MyBullet *>(i) && dynamic_cast<MyBullet *>(i)->getDir() == -1) {
-				if (dynamic_cast<MyBullet *>(i)->sceneBoundingRect().y() +
-				    dynamic_cast<MyBullet *>(i)->pixmap().height() / 2 >
-				    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
-					up = false;
-				}
-				else {
-					down = false;
-				}
-				if (dynamic_cast<MyBullet *>(i)->sceneBoundingRect().x() +
-				    dynamic_cast<MyBullet *>(i)->pixmap().width() / 2 >
-				    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
-					right = false;
-				}
-				else {
-					left = false;
+				else if (dynamic_cast<MyShipShield *>(i) && i->isVisible() && i != shp->shpshld) {
+					if (dynamic_cast<MyShipShield *>(i)->sceneBoundingRect().y() +
+					    dynamic_cast<MyShipShield *>(i)->pixmap().height() / 2 >
+					    shp->sceneBoundingRect().y() + shp->pixmap().height() / 2) {
+						up = false;
+					}
+					else {
+						down = false;
+					}
+					if (dynamic_cast<MyShipShield *>(i)->sceneBoundingRect().x() +
+					    dynamic_cast<MyShipShield *>(i)->pixmap().width() / 2 >
+					    shp->sceneBoundingRect().x() + shp->pixmap().width() / 2) {
+						right = false;
+					}
+					else {
+						left = false;
+					}
 				}
 			}
 		}
@@ -136,23 +180,23 @@ void MyAi::gnrt_cmmnds() {
 	if (!up && cmmnds->find(-Qt::Key_W) != cmmnds->end()) {
 		cmmnds->remove(-Qt::Key_W);
 	}
-	if (up && !down) {
-		cmmnds->insert(-Qt::Key_W);
-	}
-	if (!up && down) {
-		cmmnds->insert(-Qt::Key_S);
-	}
-
 	if (!right && cmmnds->find(-Qt::Key_D) != cmmnds->end()) {
 		cmmnds->remove(-Qt::Key_D);
 	}
 	if (!left && cmmnds->find(-Qt::Key_A) != cmmnds->end()) {
 		cmmnds->remove(-Qt::Key_A);
 	}
-	if (left && !right) {
+
+	if (up) {
+		cmmnds->insert(-Qt::Key_W);
+	}
+	if (down) {
+		cmmnds->insert(-Qt::Key_S);
+	}
+	if (left) {
 		cmmnds->insert(-Qt::Key_A);
 	}
-	if (!left && right) {
+	if (right) {
 		cmmnds->insert(-Qt::Key_D);
 	}
 	cmmnds->insert(-Qt::Key_X);
